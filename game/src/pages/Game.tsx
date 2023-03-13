44cebfcbc9acc01cee956254caeee0f4c016ac49
Guilherme Morone <guimorone@gmail.com>
2023-03-13 18:55:42 -0300

    game level usando context

--- game/src/pages/Game.tsx
+++ game/src/pages/Game.tsx
@@ -1,43 +1,90 @@
-import { useEffect, useState } from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { Outlet, Link, useNavigate, useOutletContext } from 'react-router-dom';
 import { ArrowLeftIcon } from '@heroicons/react/24/outline';
 import { Spinner } from 'flowbite-react';
-import { asyncReadLocalTxtFile, formatNumber } from '../utils';
+import { asyncReadLocalTxtFile, uniqueArray, shuffleArray } from '../utils';
+import { gameRules } from '../utils/algorithm';
+import {
+  MAX_LEVEL,
+  INITIAL_PLAYER_HEALTH,
+  INITIAL_BOSS_HEALTH,
+  INITIAL_MIN_WORDS_LENGTH,
+  INITIAL_MAX_WORDS_LENGTH,
+} from '../constants';
 import conjugations from '../assets/words/conjugations.txt';
 import dicio from '../assets/words/dicio.txt';
 import verbs from '../assets/words/verbs.txt';
 import words from '../assets/words/words.txt';
 
-export default function Game() {
-  const [loading, setLoading] = useState<boolean>(true);
-  const [conjugationsList, setConjugationsList] = useState<string[]>([]);
-  const [dicioList, setDicioList] = useState<string[]>([]);
-  const [verbsList, setVerbsList] = useState<string[]>([]);
-  const [wordsList, setWordsList] = useState<string[]>([]);
+type GameStatusOptions = 'starting' | 'pause' | 'running' | 'victory' | 'defeat' | 'done';
 
-  // Depends on level
-  const wordsMinLength = 3;
-  const wordsMaxLength = 6;
+type OutletContextType = {
+  words: string[];
+  gameStatus: GameStatusOptions;
+  playerHealth: number;
+  setPlayerHealth: Dispatch<SetStateAction<number>>;
+  bossHealth: number;
+  setBossHealth: Dispatch<SetStateAction<number>>;
+};
+
+export default function Game() {
+  const navigate = useNavigate();
+
+  const [gameStatus, setGameStatus] = useState<GameStatusOptions>('starting');
+  const [level, setLevel] = useState<number>(0);
+  const [playerHealth, setPlayerHealth] = useState<number>(INITIAL_PLAYER_HEALTH);
+  const [bossHealth, setBossHealth] = useState<number>(INITIAL_BOSS_HEALTH);
+  const [wordsMinLength, setWordsMinLength] = useState<number>(INITIAL_MIN_WORDS_LENGTH);
+  const [wordsMaxLength, setWordsMaxLength] = useState<number>(INITIAL_MAX_WORDS_LENGTH);
+  const [wordsList, setWordsList] = useState<string[]>([]);
 
   // load all words
   useEffect(() => {
-    const conjPromise = asyncReadLocalTxtFile(conjugations);
-    const dicioPromise = asyncReadLocalTxtFile(dicio);
-    const verbsPromise = asyncReadLocalTxtFile(verbs);
-    const wordsPromise = asyncReadLocalTxtFile(words);
+    switch (gameStatus) {
+      case 'starting':
+        const conjPromise = asyncReadLocalTxtFile(conjugations);
+        const dicioPromise = asyncReadLocalTxtFile(dicio);
+        const verbsPromise = asyncReadLocalTxtFile(verbs);
+        const wordsPromise = asyncReadLocalTxtFile(words);
 
-    Promise.all([conjPromise, dicioPromise, verbsPromise, wordsPromise])
-      .then(([conjData, dicioData, verbsData, wordsData]) => {
-        setConjugationsList(
-          conjData.filter((str: string) => str.length >= wordsMinLength && str.length <= wordsMaxLength)
-        );
-        setDicioList(dicioData.filter((str: string) => str.length >= wordsMinLength && str.length <= wordsMaxLength));
-        setVerbsList(verbsData.filter((str: string) => str.length >= wordsMinLength && str.length <= wordsMaxLength));
-        setWordsList(wordsData.filter((str: string) => str.length >= wordsMinLength && str.length <= wordsMaxLength));
-      })
-      .catch(err => console.error(`Error in "load all words" step!\n${err}`))
-      .finally(() => setLoading(false));
-  }, []);
+        Promise.all([conjPromise, dicioPromise, verbsPromise, wordsPromise])
+          .then(([conjData, dicioData, verbsData, wordsData]) => {
+            setWordsList(uniqueArray([...conjData, ...dicioData, ...verbsData, ...wordsData]));
+          })
+          .catch(err => console.error(`Error in "load all words" step!\n${err}`))
+          .finally(() => setLevel(1));
+        break;
+      case 'victory':
+        if (level >= MAX_LEVEL) setGameStatus('done');
+        else setLevel(prevLevel => prevLevel + 1);
+        break;
+      case 'done':
+        alert('max level reached');
+        navigate('/');
+        break;
+      default:
+        break;
+    }
+  }, [gameStatus]);
+
+  useEffect(() => {
+    if (level > 0) {
+      if (level > 1) {
+        const { newBossHealth, minWordsLength, maxWordsLength } = gameRules(level);
+        setBossHealth(newBossHealth);
+        setWordsMinLength(minWordsLength);
+        setWordsMaxLength(maxWordsLength);
+      }
+
+      setGameStatus('running');
+      navigate(level.toString());
+    }
+  }, [level]);
+
+  useEffect(() => {
+    if (bossHealth <= 0) setGameStatus('victory');
+    else if (playerHealth <= 0) setGameStatus('defeat');
+  }, [playerHealth, bossHealth]);
 
   return (
     <div className="relative p-1.5 w-screen h-screen min-w-full min-h-full bg-gradient-to-t from-violet-900 to-sky-900">
@@ -48,7 +95,7 @@ export default function Game() {
         <ArrowLeftIcon aria-hidden className="w-5 h-5 sm:w-7 sm:h-7" />
         <span className="font-bold text-sm sm:text-lg">Sair</span>
       </Link>
-      {loading ? (
+      {level <= 0 ? (
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
           <div className="flex flex-col gap-y-1.5 items-center justify-center">
             <h4 className="font-medium text-lg leading-snug uppercase tracking-widest antialiased">
@@ -58,12 +105,23 @@ export default function Game() {
           </div>
         </div>
       ) : (
-        <Outlet />
+        <Outlet
+          context={{
+            words: shuffleArray(
+              wordsList.filter((str: string) => str.length >= wordsMinLength && str.length <= wordsMaxLength)
+            ),
+            gameStatus,
+            playerHealth,
+            setPlayerHealth,
+            bossHealth,
+            setBossHealth,
+          }}
+        />
       )}
-      <p>Nº Conjugações: {formatNumber(conjugationsList.length)}</p>
-      <p>Nº Dicio (??): {formatNumber(dicioList.length)}</p>
-      <p>Nº Verbos: {formatNumber(verbsList.length)}</p>
-      <p>Nº Palavras (geral): {formatNumber(wordsList.length)}</p>
     </div>
   );
+}
+
+export function useGameContext(): OutletContextType {
+  return useOutletContext<OutletContextType>();
 }
--- game/src/pages/GameLevel.tsx
+++ game/src/pages/GameLevel.tsx
@@ -1,7 +1,25 @@
 import { useParams } from 'react-router-dom';
+import { useGameContext } from './Game';
 
 export default function GameLevel() {
   const { level } = useParams();
+  const { words, gameStatus, playerHealth, setPlayerHealth, bossHealth, setBossHealth } = useGameContext();
+  const levelParsed: number = Number(level);
 
-  return <h1 className="text-9xl font-black">Level: {level}</h1>;
+  if (!words || !words.length || !level || !levelParsed || Number.isNaN(levelParsed)) return null;
+
+  return (
+    <>
+      <h1 className="text-9xl font-black">{words.length}</h1>
+      {words.slice(0, 10).map((w, index) => (
+        <p key={`ingame_word_${index}`}>{w}</p>
+      ))}
+      <button
+        className="w-fit p-1 md:px-3 md:py-1.5 flex flex-wrap gap-x-2 sm:gap-x-3.5 bg-rose-900 bg-opacity-70 hover:bg-opacity-100 rounded-full shadow-sm"
+        onClick={() => setBossHealth(0)}
+      >
+        Próximo level
+      </button>
+    </>
+  );
 }
