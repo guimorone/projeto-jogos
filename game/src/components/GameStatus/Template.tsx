06428e61590daba0a6ac1de373010ed2990dc615
gustavogaldino <gcgm@cin.ufpe.br>
2023-04-26 20:59:05 -0300

    fixing end game screen

--- game/src/components/GameStatus/Template.tsx
+++ game/src/components/GameStatus/Template.tsx
@@ -33,7 +33,7 @@ const Template: FC<IFuncProps> = ({ status, props }: IFuncProps) => {
   const header = headers[status];
 
   return (
-    <div className="space-y-5">
+    <div className="space-y-5" style={{ top: "32px", position: "absolute"}}>
       <h1
         className={classNames(
           header?.title.color,
@@ -53,77 +53,87 @@ const Template: FC<IFuncProps> = ({ status, props }: IFuncProps) => {
               {props.points} {props.points === 1 ? 'ponto' : 'pontos'}
             </h3>
           </li>
-          <li>
-            <div className="flex flex-col gap-y-2">
-              <h3 className="text-lg font-semibold">
-                {props.wordsHitsNames?.length}
-                {props.wordsHitsNames?.length === 1 ? ' palavra acertada' : ' palavras acertadas'}
-              </h3>
-              <ol className="grid grid-cols-3 lg:grid-cols-4 place-items-start gap-y-1 gap-x-12 md:gap-x-20 list-decimal">
-                {props.wordsHitsNames?.map((word: string, index: number) => (
-                  <li key={`wordsHitsNamesStatus_${word}_${index}`} className="capitalize pl-0.5">
-                    {word}
-                  </li>
-                ))}
-              </ol>
-            </div>
-          </li>
-          <li>
-            <div className="flex flex-col gap-y-2">
-              <h3 className="text-lg font-semibold">
-                {props.wordsMissedNames?.length}
-                {props.wordsMissedNames?.length === 1 ? ' palavra perdida' : ' palavras perdidas'}
-              </h3>
-              <ol className="grid grid-cols-3 lg:grid-cols-4 place-items-start gap-y-1 gap-x-12 md:gap-x-20 list-decimal">
-                {props.wordsMissedNames?.map((word: string, index: number) => (
-                  <li key={`wordsMissedNamesStatus_${word}_${index}`} className="capitalize pl-0.5">
-                    {word}
-                  </li>
-                ))}
-              </ol>
-            </div>
-          </li>
+          <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+            <li>
+              <div className="flex flex-col gap-y-2">
+                <h3 className="text-lg font-semibold">
+                  {props.wordsHitsNames?.length}
+                  {props.wordsHitsNames?.length === 1 ? ' palavra acertada' : ' palavras acertadas'}
+                </h3>
+                <ol className="grid grid-cols-3 lg:grid-cols-4 place-items-start gap-y-1 gap-x-12 md:gap-x-20 list-decimal">
+                  {props.wordsHitsNames?.map((word: string, index: number) => (
+                    <li key={`wordsHitsNamesStatus_${word}_${index}`} className="capitalize pl-0.5">
+                      {word}
+                    </li>
+                  ))}
+                </ol>
+              </div>
+            </li>
+
+            <li>
+              <div className="flex flex-col gap-y-2">
+                <h3 className="text-lg font-semibold">
+                  {props.wordsMissedNames?.length}
+                  {props.wordsMissedNames?.length === 1 ? ' palavra perdida' : ' palavras perdidas'}
+                </h3>
+                <ol className="grid grid-cols-3 lg:grid-cols-4 place-items-start gap-y-1 gap-x-12 md:gap-x-20 list-decimal">
+                  {props.wordsMissedNames?.map((word: string, index: number) => (
+                    <li key={`wordsMissedNamesStatus_${word}_${index}`} className="capitalize pl-0.5">
+                      {word}
+                    </li>
+                  ))}
+                </ol>
+              </div>
+            </li>
+          </div>
         </ul>
       </>
       {status !== 'victory' && (
         <>
           <h2 className="text-xl text-yellow-300 pb-5">Objetivos alcançados em todo jogo:</h2>
           <ul className="grid grid-cols-3 place-items-center text-left list-disc marker:text-yellow-300 min-w-full w-screen">
+
             <li>
               <h3 className="text-lg font-semibold">
                 {props.totalPoints} {props.totalPoints === 1 ? 'ponto' : 'pontos'}
               </h3>
             </li>
-            <li>
-              <div className="flex flex-col gap-y-2">
-                <h3 className="text-lg font-semibold">
-                  {props.totalWordsHitsNames?.length}
-                  {props.totalWordsHitsNames?.length === 1 ? ' palavra acertada' : ' palavras acertadas'}
-                </h3>
-                <ol className="grid grid-cols-3 lg:grid-cols-4 place-items-start gap-y-1 gap-x-12 md:gap-x-20 list-decimal">
-                  {props.totalWordsHitsNames?.map((word: string, index: number) => (
-                    <li key={`totalWordsHitsNamesStatus_${word}_${index}`} className="capitalize pl-0.5">
-                      {word}
-                    </li>
-                  ))}
-                </ol>
-              </div>
-            </li>
-            <li>
-              <div className="flex flex-col gap-y-2">
-                <h3 className="text-lg font-semibold">
-                  {props.totalWordsMissedNames?.length}
-                  {props.totalWordsMissedNames?.length === 1 ? ' palavra perdida' : ' palavras perdidas'}
-                </h3>
-                <ol className="grid grid-cols-3 lg:grid-cols-4 place-items-start gap-y-1 gap-x-12 md:gap-x-20 list-decimal">
-                  {props.totalWordsMissedNames?.map((word: string, index: number) => (
-                    <li key={`totalWordsMissedNamesStatus_${word}_${index}`} className="capitalize pl-0.5">
-                      {word}
-                    </li>
-                  ))}
-                </ol>
-              </div>
-            </li>
+
+            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+
+              <li>
+                <div className="flex flex-col gap-y-2">
+                  <h3 className="text-lg font-semibold">
+                    {props.totalWordsHitsNames?.length}
+                    {props.totalWordsHitsNames?.length === 1 ? ' palavra acertada' : ' palavras acertadas'}
+                  </h3>
+                  <ol className="grid grid-cols-3 lg:grid-cols-4 place-items-start gap-y-1 gap-x-12 md:gap-x-20 list-decimal">
+                    {props.totalWordsHitsNames?.map((word: string, index: number) => (
+                      <li key={`totalWordsHitsNamesStatus_${word}_${index}`} className="capitalize pl-0.5">
+                        {word}
+                      </li>
+                    ))}
+                  </ol>
+                </div>
+              </li>
+
+              <li>
+                <div className="flex flex-col gap-y-2">
+                  <h3 className="text-lg font-semibold">
+                    {props.totalWordsMissedNames?.length}
+                    {props.totalWordsMissedNames?.length === 1 ? ' palavra perdida' : ' palavras perdidas'}
+                  </h3>
+                  <ol className="grid grid-cols-3 lg:grid-cols-4 place-items-start gap-y-1 gap-x-12 md:gap-x-20 list-decimal">
+                    {props.totalWordsMissedNames?.map((word: string, index: number) => (
+                      <li key={`totalWordsMissedNamesStatus_${word}_${index}`} className="capitalize pl-0.5">
+                        {word}
+                      </li>
+                    ))}
+                  </ol>
+                </div>
+              </li>
+
+            </div>
           </ul>
         </>
       )}
